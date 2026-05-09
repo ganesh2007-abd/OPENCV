411666ff87e8cfe2ff05dadd6711b5de5987f365
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-09 16:31:43 +0530

    Saving Video
    1)The path file name should contain the extension .avi or something
    2)FlipCode -- 0(about X-axis),+ve(about y-axis),-ve(around both axis)
    3)Just to differentiate the saved video i used cv.flip()
    4)Refer notes for further clarity

---
+++ VIDEOS_TUTS/Saving_video.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter("D:\\SKILLS\\OPENCV\\VIDEOS_TUTS\\SAMPLE_VIDEOS\\SavedVideo.avi",fourcc,20.0,(640,480))
+
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        print("Video saved successfully")
+        break
+    frame = cv.flip(frame,0)
+    out.write(frame)
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) == ord('q'):
+        print("Interrupted")
+        break
+cap.release()
+cv.destroyAllWindows()
