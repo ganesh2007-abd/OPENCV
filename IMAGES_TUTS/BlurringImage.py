9a08ef636e84a8ec851904f0a0e70fbd7866b084
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-11 17:21:18 +0530

    Update BlurringImage.py

--- IMAGES_TUTS/BlurringImage.py
+++ IMAGES_TUTS/BlurringImage.py
@@ -1,8 +1,17 @@
 import cv2 as cv
 
 img = cv.imread("D:\SKILLS\OPENCV\IMAGES_TUTS\Sample_Imgs\Virat.jpg")
-blurred = cv.GaussianBlur(img,(5,5),cv.BORDER_DEFAULT)
 cv.imshow("Original",img)
+
+blurred = cv.GaussianBlur(img,(3,3),cv.BORDER_DEFAULT)
 cv.imshow("Blurred",blurred)
 
+average = cv.blur(img,(3,3))
+cv.imshow("Average",average)
+
+median = cv.medianBlur(img,3)
+cv.imshow("Median",median)
+
+bilateral = cv.bilateralFilter(img,5,15,15)
+cv.imshow("Bilateral",bilateral)
 cv.waitKey(0)
