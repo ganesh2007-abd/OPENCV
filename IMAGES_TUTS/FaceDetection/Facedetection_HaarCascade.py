a80c09e2672ed4a849e31bd084b410cd920da6b8
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-13 10:31:55 +0530

    FaceDetection
    HaarCascades are not the best face classifires

---
+++ IMAGES_TUTS/FaceDetection/Facedetection_HaarCascade.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("IMAGES_TUTS\Sample_Imgs\Virat.jpg")
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+face_detector = cv.CascadeClassifier("IMAGES_TUTS\FaceDetection\haar_face_def.xml")
+face_cord = face_detector.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
+
+for (x,y,w,h) in face_cord:
+    cv.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv.imshow("Faces",gray)
+
+cv.imshow("Grayed",gray)
+
+cv.waitKey(0)
