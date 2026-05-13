d096b773717a03206c34a60e08daaf8fc798793f
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-13 10:37:19 +0530

    Just some sample changes

--- IMAGES_TUTS/FaceDetection/Facedetection_HaarCascade.py
+++ IMAGES_TUTS/FaceDetection/Facedetection_HaarCascade.py
@@ -1,8 +1,9 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("IMAGES_TUTS\Sample_Imgs\Virat.jpg")
+img = cv.imread("IMAGES_TUTS\Sample_Imgs\RCB_SQUAD3.jpg")
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow("Grayed",gray)
 
 face_detector = cv.CascadeClassifier("IMAGES_TUTS\FaceDetection\haar_face_def.xml")
 face_cord = face_detector.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
@@ -12,6 +13,6 @@ for (x,y,w,h) in face_cord:
 
 cv.imshow("Faces",gray)
 
-cv.imshow("Grayed",gray)
+
 
 cv.waitKey(0)
